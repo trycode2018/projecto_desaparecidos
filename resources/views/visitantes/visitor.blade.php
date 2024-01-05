45fd3776506fc21f4fe1555f609bcb1100255365
Henrique Ventura <trycode2018@gmail.com>
2024-01-05 19:38:22 +0100

    Iniciando a pagina de visitante

---
+++ resources/views/visitantes/visitor.blade.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--- font icon css---->
+
+    <link rel="stylesheet" href="./fonts/icons/fonticons.css">
+
+    <!---swiper plugin css---->
+    <link
+    rel="stylesheet"
+    href="css/swiper-bundle-min.css"
+    />
+    <!---main style css---->
+    <link rel="stylesheet" href="css/main.css">
+    <title>Geraldbeard</title>
+</head>
+<body>
+    <header id="header" class="">
+        <nav class="container">
+            <a href="#" class="logo">Gerald<span>beard.</span></a>
+            <!--------MENU---------->
+            <div class="menu">
+                <ul class="grid">
+                    <li><a class="title" href="#home">Início</a></li>
+                    <li><a class="title" href="#about">Sobre</a></li>
+                    <li><a class="title" href="#services">Serviços</a></li>
+                    <li><a class="title" href="#testimonials">Depoimento</a></li>
+                    <li><a class="title"  href="#contact">Contacto</a></li>
+                </ul>
+            </div>
+            <!-------MENU------>
+
+                <div class="toggle icon-menu"></div>
+                <div class="toggle icon-cancel"></div>
+
+        </nav>
+    </header>
+
+    <main>
+        <!---------HOME---------->
+
+        <section class="section" id="home">
+
+            <section class="container grid" >
+                <!------------imagebanner----->
+                <div class="image">
+                    <img src="./images/img-b-1.jpg" alt="Melhor no são de belaza com ">
+                </div>
+                <!------------textbanner----->
+                <div class="text">
+                    <h2 class="title">
+                        Corte ideal para ti
+                    </h2>
+                    <p>
+                        Propomos um corte adequado ao estilo e personalidade do homem moderno.
+                    </p>
+                    <a href="#" class="button">Agendar um horário</a>
+                </div>
+            </section>
+        </section>
+
+        <div class="divider-1"></div>
+
+        <!---------ABOUT---------->
+        <section class="section" id="about" >
+            <section class="container grid" >
+                <div class="image">
+                    <img src="./images/img-b-1.jpg" alt="Melhor no são de belaza com ">
+                </div>
+                <div class="text">
+                    <h2 class="title">
+                        Sobre nós
+                    </h2>
+                    <p>
+                        É uma marca do grupo Bento Cabeleireiros, uma presença sólida no mercado da estética e beleza masculina há mais de 30 anos.
+                    </p><br>
+                    <p>
+                        Nascido a 10 anos, um conceito que surgiu da vontade de recriar um revivalismo puro das antigas, e praticamente extintas, barbearias de bairro, a Geraldbeard evoluiu e cresceu, contando hoje com 3 lojas distintas mas em tudo iguais.
+                    </p><br>
+
+                    <p><b>
+                        <i>
+                            Procure a Geraldbeard mais próxima de si e faça a sua marcação.
+                        </i>
+                    </b>
+
+                    </p>
+
+                </div>
+            </section>
+        </section>
+
+        <div class="divider-2"></div>
+
+        <!----------SERVICES------------->
+        <section class="section" id="services">
+            <div class="container grid">
+                <header>
+                    <h2 class="title">Serviços</h2>
+                    <p class="subtitle">
+                        Entre na <strong>Geraldbeard</strong> num pé e saia noutro com o visual novamente impecável.
+                    </p>
+                </header>
+                <div class="cards grid">
+                    <div class="card">
+                        <i class="icon-hair-cut"></i>
+                        <h3 class="title">Corte de cabelo</h3>
+                        <p>
+                            Com diversas técnicas de corte, requer habilidade e sensibilidade de quem o executa. Para homens contemporâneos.
+                        </p>
+                    </div>
+                    <div class="card">
+                        <i class="icon-salon"></i>
+                        <h3 class="title">Corte clássico</h3>
+                        <p>Com apenas uma lamina, tesora e pente do jeito que muito clientes gostam.</p>
+                    </div>
+                    <div class="card">
+                        <i class="icon-beard-outline"></i>
+                        <h3 class="title">Barba</h3>
+                        <p>
+                            Liberte-se da barba enquanto experiencia um momento relaxante com toalha quente.Para homens de barba rija que não descuram a sua boa aparência.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="divider-1"></div>
+
+        <!----------TESTIMONIAL---------------->
+        <section class="section" id="testimonials">
+            <div class="container">
+               <header>
+                    <h2 class="title">Depoimentos de quem já passou por aqui</h2>
+               </header>
+               <div class="testimonials swiper">
+                    <div class="swiper-wrapper">
+                        <div class="testimonial swiper-slide">
+                            <blockquote>
+                                <p>
+                                    <span>&ldquo;</span>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum assumenda voluptas numquam, molestias corporis enim? Aperiam expedita
+                                </p>
+                                <cite>
+                                    <img src="./images/danniel.png" alt="" srcset="">Daniel Geraldo
+                                </cite>
+                            </blockquote>
+                        </div>
+
+                        <div class="testimonial swiper-slide">
+                            <blockquote>
+                                <p>
+                                    <span>&ldquo;</span>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum assumenda voluptas numquam, molestias corporis enim? Aperiam expedita
+                                </p>
+                                <cite>
+                                    <img src="./images/danniel.png" alt="" srcset="">Daniel Geraldo
+                                </cite>
+                            </blockquote>
+                        </div>
+
+                        <div class="testimonial swiper-slide">
+                            <blockquote>
+                                <p>
+                                    <span>&ldquo;</span>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum assumenda voluptas numquam, molestias corporis enim? Aperiam expedita
+                                </p>
+                                <cite>
+                                    <img src="./images/danniel.png" alt="" srcset="">Daniel Geraldo
+                                </cite>
+                            </blockquote>
+                        </div>
+
+                     </div>
+
+                     <div class="swiper-pagination"></div>
+               </div>
+
+            </div>
+        </section>
+
+        <div class="divider-2"></div>
+
+        <!---------CONTACT----------->
+        <section id="contact">
+            <div class="container grid">
+                <div class="text">
+                    <h2 class="title">Entre em contacto</h2>
+                    <p>
+                        Entre em contacto com a Geraldbeard, queremos tirar suas dúvidas, ouvir suas críticas e sugestões.
+                    </p>
+                    <a href="https://api.whatsapp.com/send?phone=+244931974210&text=Oi gostaria de agendar agora?" class="button" target="_blank"><i class="icon-whatsapp"></i>Entrar em contacto</a>
+                </div>
+
+                <address class="links">
+                    <ul class="grid">
+                        <li><i class="icon-call"></i>(+244) 233-234-23</li>
+                        <li><i class="icon-envelope-o"></i>geraldbeard@barber</li>
+                        <li><i class="icon-placeholder"></i>Luanda/Cacuaco</li>
+                    </ul>
+                </address>
+            </div>
+        </section>
+
+        <div class="divider-1"></div>
+
+    </main>
+
+        <!--------FOOTER---------->
+    <footer class="section">
+        <div class="container grid">
+            <div class="brand">
+                <a href="#home" class="logo logo-alt">Gerald<span>beard.</span></a>
+                <p>2021 Geraldbeard</p>
+                <p>Todos direitos reservados.</p>
+            </div>
+            <div class="social">
+                <a href=""><i class="icon-facebook"></i></a>
+                <a href=""><i class="icon-instagram"></i></a>
+                <a href=""><i class="icon-youtube"></i></a>
+            </div>
+        </div>
+    </footer>
+
+
+    <a href="#home" class="back-to-top">
+        <img src="images/upwards-arrow.svg" alt="" srcset="">
+    </a>
+
+
+    <!---------scrollreveal--------->
+    <script src="js/scrollreveal.min.js"></script>
+
+    <!---------swiper plugin js--------->
+    <script src="js/swiper-bundle-min.js"></script>
+
+    <script src="js/main.js"></script>
+</body>
+</html>
