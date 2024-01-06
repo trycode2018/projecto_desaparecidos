33a085b6efdf804f07a0157802ae2974922484dc
lucio <luciobenguejunior47@gmail.com>
2024-01-06 14:18:54 +0100

    ajustando formulario de cadastro

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -50,7 +50,8 @@
                   <div class="row mb-3">
                     <label for="inputPassword" class="col-sm-2 col-form-label">Telefone</label>
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" name="phone_number">
+                      <input type="text" class="form-control" name="phone_number"
+                      value="{{ old('phone_number') }}">
                     </div>
                   </div>
                   <div class="row mb-3">
